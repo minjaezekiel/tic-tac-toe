fe703b2c14059c6c8133b025b4aef6d1f64f334b
Ezekiel Minja <ezekielminja@gmail.com>
2025-05-02 16:49:11 +0200

    Add files via upload
    adding functionality ch-1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Board from './components/board';
+import './App.css';
+import './style.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Board /> 
+    </div>
+  );
+}
+
+export default App;
